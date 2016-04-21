96301fb0760d4cec512776f7a4327ee3960595cd
kizzonia <chibuikeforune@hotmail.com>
2016-04-21 08:40:08 -0500

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-devise_for :users
+devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 resources :products# does not match '/products' via POST
 
   get 'contact/index'
