9da1702a3ffb4445c70fa91af6f4fe1b5a3618c9
kizzonia <chibuikeforune@hotmail.com>
2016-04-21 17:18:19 -0500

    'ckeditor'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   devise_for :admins
   devise_for :users
 resources :products# does not match '/products' via POST
-
+ mount Ckeditor::Engine => '/ckeditor'
   get 'contact/index'
   resources :posts do
  	resources :comments
