0066823950a0136f7a10a68f6db6ba4eeb7ab029
kizzonia <chibuikeforune@hotmail.com>
2016-04-21 04:16:05 -0500

    fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
