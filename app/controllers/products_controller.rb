1309b2c1732e8a75918891a995bdccb4e2674a3e
kizzonia <chibuikeforune@hotmail.com>
2016-04-22 05:26:35 -0500

    dsd

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :new, :show]
+  before_action :authenticate_user!, except: [:index, :new, :create]
   def index
     @products = Product.all.order('created_at DESC')
   end
